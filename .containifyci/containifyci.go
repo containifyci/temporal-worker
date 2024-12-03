5199135984b9423d55fb5cbd12bad25283f332b2
fr12k <frank.ittermann@yahoo.de>
2024-12-03 21:50:29 +0100

    first commit

---
+++ .containifyci/containifyci.go
@@ -0,0 +1,27 @@
+//go:generate bash -c "if [ ! -f go.mod ]; then echo 'Initializing go.mod...'; go mod init .containifyci; else echo 'go.mod already exists. Skipping initialization.'; fi"
+//go:generate go get github.com/containifyci/engine-ci/protos2
+//go:generate go get github.com/containifyci/engine-ci/client
+//go:generate go mod tidy
+
+package main
+
+import (
+	"os"
+
+	"github.com/containifyci/engine-ci/client/pkg/build"
+)
+
+func main() {
+	os.Chdir("../")
+	opts := build.NewGoServiceBuild("temporal-worker-client")
+	opts.Image = ""
+	opts.File = "client/main.go"
+
+	opts2 := build.NewGoServiceBuild("temporal-worker")
+	opts2.Image = ""
+	opts2.File = "worker/main.go"
+	opts2.Properties = map[string]*build.ListValue{
+		"goreleaser": build.NewList("true"),
+	}
+	build.Serve(opts, opts2)
+}
